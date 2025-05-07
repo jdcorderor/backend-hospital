67ebf3749d68fbd9dd995115d52359979c498c13
jdcorderor <jdcorderor@gmail.com>
2025-05-07 17:21:05 -0400

    Modified app.js

--- src/app.js
+++ src/app.js
@@ -2,27 +2,19 @@ import express, { static as stc, json } from "express";
 import morgan from "morgan";
 import cors from "cors";
 
-/*
-
-Importacion de los routers 
-
-Cada Router va a contener una parte de la api (el backend) y cada router va estar dirijido a un controlador para hacer un crud en la base de datos 
-
-*/
-
-//Administracion
+// Módulo de Administración.
 
 import ordenCompraRoutes from "./Administracion/routes/ordenCompra.routes.js";
 import productoRoutes from "./Administracion/routes/producto.routes.js";
 import recursosAdministracionRoutes from "./Administracion/routes/recursos.routes.js";
 
-//Citas
+// Módulo de Gestión de Citas.
 
 import historialRoutes from "./Citas/routes/historial.routes.js";
 import reprogramarRoutes from "./Citas/routes/reprogramar.routes.js";
 import citasRoutes from "./Citas/routes/citas.routes.js";
 
-//Compras
+// Módulo de Compras.
 
 import proveedoresRoutes from "./Compras/routes/proveedores.routes.js"
 import recursosRoutes from "./Compras/routes/recursos.routes.js";
@@ -34,7 +26,7 @@ import updateRoutes from "./Compras/routes/update.routes.js"
 import formRoutes from "./Compras/routes/form.routes.js"
 import utilsRoutes from "./Compras/routes/utils.routes.js"
 
-//Consultas Medicas
+// Módulo de Consultas Médicas.
 
 import citasMedicasRoutes from "./consultas_medicas/routes/citasMedicas.routes.js";
 import consultasMedRoutes from "./consultas_medicas/routes/consultas.routes.js";
@@ -43,7 +35,7 @@ import historialMedRoutes from "./consultas_medicas/routes/historial.routes.js";
 import insumosRoutes from "./consultas_medicas/routes/insumos.routes.js";
 import pacienteMedRoutes from "./consultas_medicas/routes/paciente.routes.js";
 
-//Consultas Odontologicas
+// Módulo de Consultas Odontológicas.
 
 import citasConsultasOdontoRoutes from "./Consultas_Odontologicas/routes/citas_consultas_odontologicas.routes.js";
 import consultasOdontologicasLabRoutes from "./Consultas_Odontologicas/routes/consultas_odontologicas.routes.js";
@@ -54,7 +46,7 @@ import sectoresRoutes from "./Consultas_Odontologicas/routes/sectores.routes.js"
 import segmentosRoutes from "./Consultas_Odontologicas/routes/segmentos.routes.js";
 import solicitudesLabRoutes from "./Consultas_Odontologicas/routes/solicitudes_lab_odontologicas.routes.js";
 
-//Hospitalizacion
+// Módulo de Hospitalización.
 
 import camasRoutes from "./Hospitalizacion/routes/camas.routes.js";
 import habitacionesRoutes from "./Hospitalizacion/routes/habitaciones.routes.js";
@@ -64,7 +56,7 @@ import signosVitalesRoutes from "./Hospitalizacion/routes/signos_vitales.routes.
 import pacientesRoutes from "./Hospitalizacion/routes/pacientes.routes.js";
 import listaEsperaRoutes from "./Hospitalizacion/routes/lista_espera_hospitalizacion.routes.js";
 
-//Inventario
+// Módulo de Inventario.
 
 import almacenRoutes from "./Inventario/routes/almacen.routes.js"
 import equiposRoutes from "./Inventario/routes/equipos.routes.js"
@@ -76,7 +68,7 @@ import productosRoutes from "./Inventario/routes/productos.routes.js";
 import productosUbicacionRoutes from "./Inventario/routes/productosUbicacion.routes.js";
 import repuestosRoutes from "./Inventario/routes/repuestos.routes.js"
 
-//Laboratorio
+// Módulo de Laboratorio.
 
 import examenes_labRoutes from "./Laboratorio/routes/examenes.routes.js";
 import pruebasRoutes from "./Laboratorio/routes/pruebas.routes.js";
@@ -84,13 +76,14 @@ import resultadosRoutes from "./Laboratorio/routes/resultados.routes.js";
 import pacienteRoutes from "./Laboratorio/routes/pacientes.routes.js";
 import solicitudesLaboratorioRoutes from "./Laboratorio/routes/solicitudes_laboratorio.routes.js";
 
-//Mantenimiento
+// Módulo de Mantenimiento.
 
 import mantenimientoRoute from './mantenimiento/routes/mantenimientoRoute.js';
 import reporteRoute from './mantenimiento/routes/reporteRoute.js'
 import ordenesTrabajoRoute from './mantenimiento/routes/ordenesRoute.js'
 
-//Personal
+// Módulo de Gestión de Personal.
+
 import empleadosRoutes from "./Personal/routes/empleados.routes.js";
 import especialidadesRoutes from "./Personal/routes/especialidades.routes.js";
 import rolesRoutes from "./Personal/routes/roles.routes.js";
@@ -99,10 +92,9 @@ import usuariosRoutes from "./Usuarios/routes/usuarios.routes.js";
 
 /*
 
-Instanciacion del servidor y configuraciones varias
+Instanciación del servidor y configuraciones varias.
 
 Esta configurado el server para enviar la pagina index.html de la carpeta public.
-Hacer la navegacion desde el front hacia las carpeta pages en un futuro
 
 */
 
@@ -110,34 +102,29 @@ import loginRoutes from "./Inicio/routes/login.routes.js"
 
 const app = express();
 
-// Middlewares
-app.use(cors());
-app.use(morgan("dev"));
-app.use(json());
-app.use(stc("public"));
+// Middlewares.
+app.use(cors()); // Gestionar solicitudes de origen cruzado.
+app.use(morgan("dev")); // Registro de solicitudes HTTP.
+app.use(json()); 
+app.use(stc("public")); // Servicio de activos estáticos de la app.
 
 /*
-
-    Declaracion de las rutas para los endpoints
-    Todas las rutas que empiecen por "/api/..." van renferenciadas al backend para no mezclar con la navegacion del front
-
-    IMPORTANTE NO USAR LA RUTA "" O "/" DIRECTAMENTE PARA QUE FUNCIONE EL DIRECCIONAMIENTO A LOS ARCHIVOS ESTATICOS EN LA CARPETA PUBLIC EN UN FUTURO
-
+    Declaración de las rutas para los endpoints.
 */
 
-//Administracion
+// Módulo de Administración.
 
 app.use("/api/administracion/ordenes", ordenCompraRoutes); // Rutas para las ordenes de compra
 app.use("/api/administracion/productos", productoRoutes); // Rutas para los productos
 app.use("/api/administracion/recursos", recursosAdministracionRoutes); // Rutas para los recursos
 
-//Citas
+// Módulo de Gestión de Citas.
 
 app.use("/api/citas/historial", historialRoutes); // Rutas para el historial de citas
 app.use("/api/citas/reprogramar", reprogramarRoutes); // Rutas para reprogramar citas
 app.use("/api/citas", citasRoutes); // Rutas generales para citas
 
-//Compras
+// Módulo de Compras.
 
 app.use("/api/compras/proveedores", proveedoresRoutes);
 app.use("/api/compras/recursos", recursosRoutes);
@@ -149,7 +136,7 @@ app.use("/api/compras/actualizar",updateRoutes)
 app.use("/api/compras/formulario",formRoutes)
 app.use("/api/compras/utiles",utilsRoutes)
 
-//Consultas Medicas
+// Módulo de Consultas Médicas.
 
 app.use("/api/consultas_medicas/citas", citasMedicasRoutes);
 app.use("/api/consultas_medicas/departamentos", departamentosRoutes);
@@ -158,7 +145,7 @@ app.use("/api/consultas_medicas/consultas", consultasMedRoutes);
 app.use("/api/consultas_medicas/historiales", historialMedRoutes);
 app.use("/api/consultas_medicas/insumos", insumosRoutes);
 
-//Consultas Odontologicas
+// Módulo de Consultas Odontológicas.
 
 app.use("/api/consultas_odontologicas/citas", citasConsultasOdontoRoutes)
 app.use("/api/consultas_odontologicas/consultas", consultasOdontologicasLabRoutes)
@@ -169,7 +156,7 @@ app.use("/api/consultas_odontologicas/sectores", sectoresRoutes)
 app.use("/api/consultas_odontologicas/segmentos", segmentosRoutes)
 app.use("/api/consultas_odontologicas/solicitudes_lab", solicitudesLabRoutes)
 
-//Hospitalizacion
+// Módulo de Hospitalización.
 
 app.use("/api/hospitalizacion/camas", camasRoutes);
 app.use("/api/hospitalizacion/habitaciones", habitacionesRoutes);
@@ -179,7 +166,7 @@ app.use("/api/hospitalizacion/signosVitales", signosVitalesRoutes);
 app.use("/api/hospitalizacion/pacientes", pacientesRoutes);
 app.use("/api/hospitalizacion/listaEspera", listaEsperaRoutes);
 
-//Inventario
+// Módulo de Inventario.
 
 app.use("/api/inventario/almacen", almacenRoutes);
 app.use("/api/inventario/equipos", equiposRoutes);
@@ -191,7 +178,7 @@ app.use("/api/inventario/modeloProductos", modeloProductosRoutes);
 app.use("/api/inventario/productosUbicacion", productosUbicacionRoutes);
 app.use("/api/inventario/repuestos", repuestosRoutes);
 
-//Laboratorio
+// Módulo de Laboratorio.
 
 app.use("/api/laboratorio/solicitudesLaboratorio", solicitudesLaboratorioRoutes);
 app.use("/api/laboratorio/pacientes", pacienteRoutes);
@@ -199,25 +186,25 @@ app.use("/api/laboratorio/resultados", resultadosRoutes);
 app.use("/api/laboratorio/examenes", examenes_labRoutes);
 app.use("/api/laboratorio/pruebas", pruebasRoutes);
 
-//Mantenimiento
+// Módulo de Mantenimiento.
 
 app.use('/api/mantenimiento/mantenimiento', mantenimientoRoute);
 app.use('/api/mantenimiento/ordenes', ordenesTrabajoRoute);
 app.use('/api/mantenimiento/reporte', reporteRoute);
 
-//Personal
+// Módulo de Gestión de Personal.
 
 app.use('/api/personal/empleados', empleadosRoutes);
 app.use("/api/personal/especialidades", especialidadesRoutes);
 app.use("/api/personal/roles", rolesRoutes);
 app.use("/api/personal/pagos_empleados", pagosEmpleadosRoutes);
 
-//Usuarios
-app.use("/api/usuarios", usuariosRoutes);
+// Usuarios y login.
 
+app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/iniciar",loginRoutes)
 
-// Middleware para manejar rutas no encontradas
+// Middleware para manejar rutas no encontradas.
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
 });
